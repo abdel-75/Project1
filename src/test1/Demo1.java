4d009a281b236a6c745633e83b835a1719a1b8dd
Abdessalam <Abdessalam@192.168.148.117>
2023-04-06 02:38:51 +0000

    initial commit

---
+++ src/test1/Demo1.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		System.out.println("project 1......");
+		
+		System.out.println("Batch9 Git Lecture....");
+	}
+
+}
